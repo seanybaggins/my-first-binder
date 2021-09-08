9d170df53ad01e2a052463bbbe82e57fc2b9641f
Sean Link <seanybaggins1@gmail.com>
2021-09-07 20:22:32 -0600

    Created a more simple example for testing interactive webpage

--- hello.py
+++ hello.py
@@ -1,19 +1,6 @@
 from ipywidgets import interact
-import numpy as np
-import random
 
-PRIZES = ['Car', 'Goat 1', 'Goat 2']
+def square(x):
+    return x * x
 
-def monty_hall(example_num=0):
-    '''
-    Simulates one round of the Monty Hall Problem. Outputs a tuple of
-    (result if stay, result if switch, result behind opened door) where
-    each results is one of PRIZES.
-    '''
-    pick = random.choice(PRIZES)
-    opened = random.choice(
-        [p for p in PRIZES if p != pick and p != 'Car']
-    )
-    remainder = next(p for p in PRIZES if p != pick and p != opened)
-    return (pick, remainder, opened)
-
+interact(square, x=10)
