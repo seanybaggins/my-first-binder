67d7900dccf5705118991db3f0f0dcf555ae659a
Sean Link <seanybaggins1@gmail.com>
2021-09-07 20:15:32 -0600

    Attempting to run my own widget

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print("Hello from Binder!")
+from ipywidgets import interact
+import numpy as np
+import random
+
+PRIZES = ['Car', 'Goat 1', 'Goat 2']
+
+def monty_hall(example_num=0):
+    '''
+    Simulates one round of the Monty Hall Problem. Outputs a tuple of
+    (result if stay, result if switch, result behind opened door) where
+    each results is one of PRIZES.
+    '''
+    pick = random.choice(PRIZES)
+    opened = random.choice(
+        [p for p in PRIZES if p != pick and p != 'Car']
+    )
+    remainder = next(p for p in PRIZES if p != pick and p != opened)
+    return (pick, remainder, opened)
+
